d60b2cf2aebe772c1ca631b76da5b17bb28c7d67
filesmuggler <krzste09@outlook.com>
2023-01-09 10:25:18 +0100

    added docs to Agent.py

--- src/sneaky_snakes/Agent.py
+++ src/sneaky_snakes/Agent.py
@@ -11,6 +11,13 @@ LR = 0.001
 
 class Agent:
     def __init__(self, batch: int, max_mem: int, lr: float):
+        '''
+        Agent constructor
+        Args:
+            batch: number of samples to use in one batch training
+            max_mem: maximal number of past states stored in the memory of the agent
+            lr: learning rate
+        '''
         self.batch_size = batch
         self.max_mem = max_mem
         self.lr = lr
@@ -22,6 +29,23 @@ class Agent:
         #TODO: model and trainer
 
     def get_state(self, environment: GameAI) -> np.array:
+        '''
+        Returns the state of the environment - the obstacles in every direction,
+        current direction of the agent and if food is nearby.
+        State vector [0,1,0,1,0,0,0,0,0,0,0] -> snake going left with
+        danger to the right from the head, no food to be seen nearby.
+        Look for the ascii image below as a reference.
+        _________________
+        |     ########  |
+        |               |
+        | @             |
+        |_______________|
+        Args:
+            environment: game environment
+
+        Returns:
+            state vector with 11 int values
+        '''
         head = environment.snake.get_head()
 
         #TODO: calculating state
@@ -29,6 +53,18 @@ class Agent:
         return np.array(state,dtype=int)
 
     def save_to_memory(self, current_state: np.array, action, reward, next_state, done):
+        '''
+        Saves set of states, action, reward and status of the iteration to the memory
+        Args:
+            current_state: state the agent is coming from
+            action: action he takes to go to the next state
+            reward: reward to get in the next state
+            next_state: the next state
+            done: if the game is over or not
+
+        Returns:
+            void
+        '''
         data_tuple = (current_state,action,reward,next_state,done)
         self.memory.append(data_tuple)
 
